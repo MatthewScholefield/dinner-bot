2ddc9564ab603a78dff53953192fb63167c8b8f9
Matthew D. Scholefield <matthew331199@gmail.com>
2019-03-31 23:41:01 -0500

    Make tk import optional

--- dinner_bot/tk_bot.py
+++ dinner_bot/tk_bot.py
@@ -2,64 +2,66 @@ from argparse import ArgumentParser
 
 from dinner_bot.bot_frontend import BotFrontend
 
-from tkinter import *
-
 
 class ScrollTxtArea:
     def __init__(self, root):
-        frame = Frame(root)
+        import tkinter as tk
+        frame = tk.Frame(root)
         frame.grid(row=1, column=0, columnspan=3, pady=10)
         # add a frame and put a text area into it
-        textPad = Frame(frame)
-        self.text = Text(textPad, height=5, width=40)
+        padding = tk.Frame(frame)
+        self.text = tk.Text(padding, height=5, width=40)
 
         # add a vertical scroll bar to the text area
-        scroll = Scrollbar(textPad)
+        scroll = tk.Scrollbar(padding)
         self.text.configure(yscrollcommand=scroll.set)
 
         # pack everything
-        self.text.pack(side=LEFT)
-        scroll.pack(side=RIGHT, fill=Y)
-        textPad.pack(side=TOP)
+        self.text.pack(side=tk.LEFT)
+        scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        padding.pack(sidetk=tk.TOP)
 
 
 class TkBot(BotFrontend):
     cur_user = 1
 
+    @staticmethod
     def register_args(parser: ArgumentParser):
         parser.add_argument('--tk', action='store_true')
 
     def __init__(self, args, core_bot):
         super().__init__(core_bot)
-        self.user_id = self.cur_user
-        self.cur_user += 1
-
         if not args.tk:
             raise NotImplementedError
 
-        self.top = tk = Tk()
-        l1 = Label(tk, text="Enter Query")
+        import tkinter as tk
+        self.user_id = self.cur_user
+        self.cur_user += 1
+        self.rt = rt = tk.Tk()
+        l1 = tk.Label(rt, text="Enter Query")
         l1.grid(row=0, column=0)
-        self.entry = Entry(tk, bd=5)
+        self.entry = tk.Entry(rt, bd=5)
         self.entry.grid(row=0, column=1)
         self.entry.bind('<Return>', self.submit)
 
-        but = Button(tk, text="Submit", width=10, command=self.submit)
+        but = tk.Button(rt, text="Submit", width=10, command=self.submit)
         but.grid(row=0, column=2)
         self.buffer_lines = []
-        self.scroll = ScrollTxtArea(tk)
-        self.scroll.text.delete(1.0, END)
+        self.scroll = ScrollTxtArea(rt)
+        self.scroll.text.delete(1.0, tk.END)
 
     def submit(self, event=None):
+        import tkinter as tk
         def reply(x):
             self.buffer_lines.append(x)
-            self.scroll.text.delete(1.0, END)
-            self.scroll.text.insert(END, '\n'.join(self.buffer_lines))
+            self.scroll.text.delete(1.0, tk.END)
+            self.scroll.text.insert(tk.END, '\n'.join(self.buffer_lines))
+
         inp = self.entry.get()
         reply('> ' + inp)
         user_id = 'console_user_{}'.format(self.user_id)
         self.give_message(inp, user_id, user_id, reply)
-        self.entry.delete(0, END)
+        self.entry.delete(0, tk.END)
 
     def run(self):
-        self.top.mainloop()
+        self.rt.mainloop()
