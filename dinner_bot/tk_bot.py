52d18946da1f3cb65f3b618395ee028b15c984d4
Matthew D. Scholefield <matthew331199@gmail.com>
2019-04-22 01:01:42 -0500

    Make tk_bot support multiple windows

--- dinner_bot/tk_bot.py
+++ dinner_bot/tk_bot.py
@@ -1,4 +1,7 @@
+from time import sleep
+
 from argparse import ArgumentParser
+from threading import Thread
 
 from dinner_bot.bot_frontend import BotFrontend
 
@@ -10,7 +13,7 @@ class ScrollTxtArea:
         frame.grid(row=1, column=0, columnspan=3, pady=10)
         # add a frame and put a text area into it
         padding = tk.Frame(frame)
-        self.text = tk.Text(padding, height=5, width=40)
+        self.text = tk.Text(padding, height=10, width=80)
 
         # add a vertical scroll bar to the text area
         scroll = tk.Scrollbar(padding)
@@ -19,36 +22,35 @@ class ScrollTxtArea:
         # pack everything
         self.text.pack(side=tk.LEFT)
         scroll.pack(side=tk.RIGHT, fill=tk.Y)
-        padding.pack(sidetk=tk.TOP)
+        padding.pack(side=tk.TOP)
 
 
-class TkBot(BotFrontend):
-    cur_user = 1
-
-    @staticmethod
-    def register_args(parser: ArgumentParser):
-        parser.add_argument('--tk', action='store_true')
-
-    def __init__(self, args, core_bot):
-        super().__init__(core_bot)
-        if not args.tk:
-            raise NotImplementedError
-
+class TkDinnerBotWindow:
+    def __init__(self, root, user_id, give_message):
         import tkinter as tk
-        self.user_id = self.cur_user
-        self.cur_user += 1
-        self.rt = rt = tk.Tk()
-        l1 = tk.Label(rt, text="Enter Query")
+        self.tk = tk
+        self.root = root
+        root.winfo_toplevel().title("Dinner Bot {}".format(user_id))
+        self.user_id = user_id
+        self.root = root
+        l1 = tk.Label(root, text="Enter Query", width=20)
         l1.grid(row=0, column=0)
-        self.entry = tk.Entry(rt, bd=5)
+        self.entry = tk.Entry(root, bd=2, width=40)
         self.entry.grid(row=0, column=1)
         self.entry.bind('<Return>', self.submit)
 
-        but = tk.Button(rt, text="Submit", width=10, command=self.submit)
+        but = tk.Button(root, text="Submit", width=20, command=self.submit)
         but.grid(row=0, column=2)
         self.buffer_lines = []
-        self.scroll = ScrollTxtArea(rt)
+        self.scroll = ScrollTxtArea(root)
         self.scroll.text.delete(1.0, tk.END)
+        self.give_message = give_message
+
+    def is_open(self):
+        try:
+            return self.root.state() == 'normal'
+        except self.tk.TclError:
+            return False
 
     def submit(self, event=None):
         import tkinter as tk
@@ -63,5 +65,30 @@ class TkBot(BotFrontend):
         self.give_message(inp, user_id, user_id, reply)
         self.entry.delete(0, tk.END)
 
+
+class TkBot(BotFrontend):
+    @staticmethod
+    def register_args(parser: ArgumentParser):
+        parser.add_argument('--tk-windows', type=int, default=0)
+
+    def __init__(self, args, core_bot):
+        super().__init__(core_bot)
+        if not args.tk_windows:
+            raise NotImplementedError
+
+        import tkinter as tk
+        self.root = tk.Tk()
+        self.root.withdraw()
+        self.windows = []
+        for i in range(1, args.tk_windows + 1):
+            self.windows.append(TkDinnerBotWindow(tk.Toplevel(self.root), i, self.give_message))
+
+    def monitor(self):
+        while True:
+            sleep(0.5)
+            if not any(i.is_open() for i in self.windows):
+                self.root.destroy()
+
     def run(self):
-        self.rt.mainloop()
+        Thread(target=self.monitor, daemon=True).start()
+        self.root.mainloop()
